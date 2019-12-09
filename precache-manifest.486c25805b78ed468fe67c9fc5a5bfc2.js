c7baa231afa196f59ac4b5b23462f17e927e00c8
Daniel Rauf <drauf@atlassian.com>
2019-12-09 11:48:25 +0100

    Updates

---
+++ precache-manifest.486c25805b78ed468fe67c9fc5a5bfc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27e4efc7b734858ac12",
+    "url": "/watson/static/css/main.6e4c32b5.chunk.css"
+  },
+  {
+    "revision": "c27e4efc7b734858ac12",
+    "url": "/watson/static/js/main.e7420839.chunk.js"
+  },
+  {
+    "revision": "a5c5f145c1c3681e50e1",
+    "url": "/watson/static/js/runtime~main.f0630cf3.js"
+  },
+  {
+    "revision": "dca295b731ce2c38da70",
+    "url": "/watson/static/js/2.9d5e4b4a.chunk.js"
+  },
+  {
+    "revision": "985c91214ef9a9808e5bcc7198e465c3",
+    "url": "/watson/index.html"
+  }
+];
